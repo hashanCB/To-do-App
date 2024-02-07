7bea03869dfc680d612cbb03ed44de023e9f52d4
Hashan <hashan.chanaka96@gmail.com>
2024-02-07 22:35:26 +0000

    updated

--- app/components/Add_Task.jsx
+++ app/components/Add_Task.jsx
@@ -6,9 +6,8 @@ const Add_Task = () => {
       <div className=" w-full px-8  gap-4 pt-[30px] flex ">
         <input
           type="text"
-          value=""
           placeholder="Enter your task"
-          className=" w-full h-[40px] rounded-md pl-5"
+          className=" w-full h-[40px] rounded-md pl-5 focus:outline-none"
         />
         <div className=" bg-red-400 min-w-[100px] py-2 text-center rounded-lg cursor-pointer ">
           <input

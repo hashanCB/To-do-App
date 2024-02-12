601622d416ebb5eb1d4a048b7f5ff12da33d8920
Hashan <hashan.chanaka96@gmail.com>
2024-02-12 11:56:19 +0000

    better rect form hook than formik SO , rect hook use and yup using validetion

--- app/components/Add_Task.jsx
+++ app/components/Add_Task.jsx
@@ -1,63 +1,43 @@
 "use client";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+
 import React, { useRef, useState } from "react";
-import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Schema } from "../utils/Validetion/UserValidation";
 
-const Add_Task = ({ addtask }) => {
-  const [addTodo, setAddTodo] = useState(" ");
-  const refInput = useRef();
+const Add_Task = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(Schema),
+  });
 
-  const setTodo = (valuename) => {
-    if (valuename === "") {
-      console.log("error");
-    } else {
-      setAddTodo(valuename);
-    }
-  };
-  const handleb = () => {
-    setAddTodo("");
-    if (addTodo.trim() === "") {
-      console.log("enter value red");
-      // refInput.current.focus();
-    } else {
-      addtask(addTodo);
-      refInput.current.style.borderColor = "";
-    }
+  const onSubmit = (data) => {
+    console.log(data);
   };
   return (
     <div className=" w-full">
-      <Formik
-        initialValues={{ input: "" }}
-        validationSchema={Yup.object({
-          input: Yup.string().required("*Required"),
-        })}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        <Form>
-          <div className=" w-full px-8  gap-4 pt-[30px] flex ">
-            <div className=" flex flex-col w-full">
-              <Field
-                type="text"
-                name="inputname"
-                className=" w-full h-[40px] rounded-md pl-5 "
-              />
-              <ErrorMessage
-                name="input"
-                className=" text-red-500"
-                component="div"
-              />
-            </div>
-
-            <div className=" bg-red-400 min-w-[100px] h-[42px] py-2 text-center rounded-lg cursor-pointer ">
-              <button type="submit" className=" text-white font-semibold ">
-                Submit
-              </button>
-            </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className=" w-full px-8  gap-4 pt-[30px] flex ">
+          <div className=" flex flex-col w-full">
+            <input
+              type="text"
+              name="inputname"
+              className=" w-full h-[40px] rounded-md pl-5 "
+              {...register("inputname")}
+            />
+            <p className=" text-white"> {errors.inputname?.message}</p>
           </div>
-        </Form>
-      </Formik>
+
+          <div className=" bg-red-400 min-w-[100px] h-[42px] py-2 text-center rounded-lg cursor-pointer ">
+            <button type="submit" className=" text-white font-semibold ">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
